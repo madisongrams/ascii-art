6a06d8b89dc112907d79fdfa96ea1488c29ac33f
Madison Grams <madisongrams.dev@gmail.com>
2024-02-09 16:15:40 -0500

    updating algorithm

--- main.py
+++ main.py
@@ -1,38 +1,50 @@
 from PIL import Image
 
-characters = "@QB#NgWM8RDHdOKq9$6khEPXwmeZaoS2yjufF]}{tx1zv7lciL/\\|?*>r^;:_\"~,'.-`"
-
+characters = " .:-=+*#%@"
 
 def convert_pixel(px):
-  return px[0] + px[1] + px[2] / 3
+  return 0.2125 * px[0] + 0.7154 * px[1] + 0.0721 * px[2]
+
+
+def get_character(x, y, im):
+  brightness_sum = 0
+  for px in range(x, x + 2):
+    for py in range(y, y + 3):
+      if (px > im.width - 1):
+        px = im.width - 1
+      if (py > im.height - 1):
+        py = im.height - 1
+      pixel = im.getpixel((px, py))
+      brightness_sum += convert_pixel(pixel)
+
+  brightness_sum /= 6
+  proportion = brightness_sum / 255
+  index = round(len(characters) * proportion)
+
+  if (index >= len(characters)):
+    index = len(characters) - 1
+
+  if (index < 0):
+    index = 0
+
+  return characters[index]
 
 
 def make_ascii_art(im):
-  for y in range(0, im.height):
+  # we will convert every group of 6 pixels into one character
+  for y in range(0, im.height, 3):
     row = ''
-    for x in range(0, im.width):
-      px = im.getpixel((x, y))
-      brightness = convert_pixel(px)
-
-      proportion = brightness / 256
-      index = round(len(characters) * proportion)
-      if (index >= len(characters)):
-        index = len(characters) - 1
-
-      if (index < 0):
-        index = 0
-
-      row += characters[index] * 3
+    for x in range(0, im.width, 2):
+      row += get_character(x, y, im)
 
     print(row)
 
 
-def open_image(file_name='blackbird.jpg'):
+def open_image(file_name='man.jpg'):
   im = Image.open(file_name)
   width = im.width
   height = im.height
-
-  newWidth = 70
+  newWidth = 300
   newHeight = round(newWidth * (height / width))
   return im.resize((newWidth, newHeight))
 
